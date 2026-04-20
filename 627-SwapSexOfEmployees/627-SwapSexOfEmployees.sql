e69145b38fff0279c21c65f9863a0530e55d6332
Akshit Bhalla <akshitbhalla2@gmail.com>
2026-04-19 23:23:05 -0700

    Create 627-SwapSexOfEmployees

---
+++ 627-SwapSexOfEmployees/627-SwapSexOfEmployees.sql
@@ -0,0 +1,10 @@
+-- Last updated: 4/19/2026, 11:23:04 PM
+# Write your MySQL query statement below
+
+UPDATE Salary
+SET 
+    sex = CASE 
+        WHEN sex = "m" 
+        THEN "f"
+        ELSE "m"
+    END 
