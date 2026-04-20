bb6bdd8941f5cfa42d4d33e206195dbc84b0cd40
Akshit Bhalla <akshitbhalla2@gmail.com>
2026-04-19 23:23:46 -0700

    Create 91-DecodeWays

---
+++ 91-DecodeWays/91-DecodeWays.py
@@ -0,0 +1,25 @@
+# Last updated: 4/19/2026, 11:23:45 PM
+class Solution:
+    def numDecodings(self, s: str) -> int:
+
+        if (not s) or (s[0] == "0"):
+            return 0
+
+        n = len(s)
+        dp = [0] * (n+1)
+
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, n+1):
+            if 1 <= int(s[i-1]) <= 9:
+                dp[i] += dp[i-1]
+            
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+
+        return dp[n]
+
+
+        
+        
