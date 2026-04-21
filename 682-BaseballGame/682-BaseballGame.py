40729f6c0abf8c3b5acf9f761898aed0d14f6dbd
Akshit Bhalla <akshitbhalla2@gmail.com>
2026-04-20 20:30:41 -0700

    Create 682-BaseballGame

---
+++ 682-BaseballGame/682-BaseballGame.py
@@ -0,0 +1,16 @@
+# Last updated: 4/20/2026, 8:30:40 PM
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        
+        scores = []
+        for op in operations:
+            if op == "C":
+                scores.pop()
+            elif op == "D":
+                scores.append(scores[-1] * 2)
+            elif op == "+":
+                scores.append(scores[-1] + scores[-2])
+            else:
+                scores.append(int(op))
+        
+        return sum(scores)
